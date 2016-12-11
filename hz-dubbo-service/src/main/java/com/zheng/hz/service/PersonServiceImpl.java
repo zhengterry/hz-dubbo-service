62fd19c983a0c2cb384305ce01001aba03d32ca4
hz <602175197@qq.com>
2016-12-11 13:05:00 +0800

    添加日志、单元测试、

--- hz-dubbo-service/src/main/java/com/zheng/hz/service/PersonServiceImpl.java
+++ hz-dubbo-service/src/main/java/com/zheng/hz/service/PersonServiceImpl.java
@@ -4,6 +4,7 @@ import com.zheng.hz.api.model.PersonVO;
 import com.zheng.hz.api.service.PersonService;
 import com.zheng.hz.dao.auto.PersonMapper;
 import com.zheng.hz.model.auto.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by Administrator on 2016/11/13/013.
  */
+@Slf4j
 @Service
 public class PersonServiceImpl implements PersonService {
 
